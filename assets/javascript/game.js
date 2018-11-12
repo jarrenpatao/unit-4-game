da822a953e5657600c3c1ee1e5b2f7313b31f639
jarrenpatao <jarrenpatao@gmail.com>
2018-11-12 14:27:27 -0800

    just added frills to the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 var targetNumber = Math.floor(Math.random() * 80) + 20;
+
   $("#number-to-guess").text(targetNumber);
   var counter = 0;
   var crystalPics = ["assets/images/GT1Blk.png", "assets/images/GT3Blu.png", "assets/images/GT2Red.png", "assets/images/GT4Gre.png"]
@@ -7,9 +8,7 @@ var targetNumber = Math.floor(Math.random() * 80) + 20;
     targetNumber = Math.floor(Math.random() * 80) + 20;
     $("#number-to-guess").text(targetNumber);
   }
-
-
-  
+ 
   for (var i = 0; i < crystalPics.length; i++) {
     var numberOptions = Math.floor(Math.random() * 15) + 2;
     var imageCrystal = $("<img>");
@@ -27,6 +26,10 @@ var targetNumber = Math.floor(Math.random() * 80) + 20;
 
     if (counter === targetNumber) {
       alert("You win!");
+      var celeb = $("<img>");
+      var celebPics = ["https://media.giphy.com/media/5Yl08OSg4AckeNHpDc/giphy.gif"];
+      celeb.attr("src", celebPics);
+      $("#celebrate").append(celeb);
       reset();
     }
     else if (counter >= targetNumber) {
