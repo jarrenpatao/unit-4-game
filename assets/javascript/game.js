5dc3c0168bea27853a9f647e28aff247d0db4249
jarrenpatao <jarrenpatao@gmail.com>
2018-11-11 17:51:44 -0800

    changed the math for the targetNumber so that it wouldn't always be 100

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,21 @@
-// Game Goal
-var targetNumber = 100;
+var targetNumber = Math.floor(Math.random() * 100) + 2;
   $("#number-to-guess").text(targetNumber);
   var counter = 0;
-  var numberOptions = [[Math.floor(Math.random() * 15) + 2]];
-  for (var i = 0; i < numberOptions.length; i++) {
-    $("imageCrystal");{
-    var imageCrystal1 = $("<img>");
-    var imageCrystal2 = $("<img>");
-    var imageCrystal3 = $("<img>");
-    var imageCrystal4 = $("<img>");
-    }
+  // var numberOptions = [[Math.floor(Math.random() * 15) + 2]];
+  var crystalPics = ["assets/images/GT1Blk.png", "assets/images/GT3Blu.png", "assets/images/GT2Red.png", "assets/images/GT4Gre.png"]
 
+
+  
+  for (var i = 0; i < crystalPics.length; i++) {
+    var numberOptions = Math.floor(Math.random() * 15) + 2;
+    var imageCrystal = $("<img>");
     imageCrystal.addClass("crystal-image");
-    imageCrystal1.attr("src", "assets/images/GT1Blk.png");
-    imageCrystal2.attr("src", "assets/images/GT3Blu.png");
-    imageCrystal3.attr("src", "assets/images/GT2Red.png");
-    imageCrystal4.attr("src", "assets/images/GT4Gre.png");
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    imageCrystal.attr("src", crystalPics[i]);
+    imageCrystal.attr("data-crystalvalue", numberOptions);
     $("#crystals").append(imageCrystal);
   }
 
   $(".crystal-image").on("click", function() {
-
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
@@ -30,7 +24,6 @@ var targetNumber = 100;
     if (counter === targetNumber) {
       alert("You win!");
     }
-
     else if (counter >= targetNumber) {
       alert("You lose!!");
     }
